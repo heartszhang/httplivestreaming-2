f3eb06ab2a741dc8d0d03f361fae60a14608c8eb
hearts <hearts.zhang@gmail.com>
2015-04-14 18:15:46 +0800

    - reconfig midl and custom build step

--- demo.dot/demo.dot_h.h
+++ demo.dot/demo.dot_h.h
@@ -4,10 +4,10 @@
 
 
  /* File created by MIDL compiler version 8.00.0603 */
-/* at Sat Apr 11 11:56:03 2015
+/* at Tue Apr 14 18:13:11 2015
  */
-/* Compiler settings for C:\Users\Hearts\AppData\Local\Temp\demo.dot.idl-1f318b3b:
-    Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 8.00.0603 
+/* Compiler settings for C:\Users\Hearts\AppData\Local\Temp\demo.dot.idl-bd4227e7:
+    Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.00.0603 
     protocol : dce , ms_ext, c_ext, robust
     error checks: allocation ref bounds_check enum stub_data 
     VC __declspec() decoration level: 
